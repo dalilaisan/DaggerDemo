940232c8edf607a55fdbb52d0aeeffe4046f2b8b
Dalila Isanovic <dalila.isan@gmail.com>
2021-05-20 01:56:27 +0200

    Initial commit - created app component

---
+++ app/src/main/java/com/example/dalila/daggerdemo/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.dalila.daggerdemo;
+
+import com.example.dalila.daggerdemo.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/example/dalila/daggerdemo/di/AppComponent.java
@@ -0,0 +1,23 @@
+package com.example.dalila.daggerdemo.di;
+
+import android.app.Application;
+
+import com.example.dalila.daggerdemo.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(modules = AndroidSupportInjectionModule.class)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application (Application application);
+
+        AppComponent build();
+    }
+}
