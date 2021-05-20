46dd907218c53ee53b89139d8819f0f86484a440
Dalila Isanovic <dalila.isan@gmail.com>
2021-05-20 02:22:00 +0200

    added appModule to AppComponent

--- app/src/main/java/com/example/dalila/daggerdemo/di/AppComponent.java
+++ app/src/main/java/com/example/dalila/daggerdemo/di/AppComponent.java
@@ -10,7 +10,8 @@ import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
 @Component(modules = { AndroidSupportInjectionModule.class,
-                     ActivityBuildersModule.class} )
+                     ActivityBuildersModule.class,
+                    AppModule.class} )
 public interface AppComponent extends AndroidInjector<BaseApplication> {
 
     @Component.Builder
